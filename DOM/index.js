01eef95f6bab78056fcf351daa1a1a604376911a
ashutosh2329 <sonutiwari2329@gmail.com>
2020-07-30 00:23:06 +0530

    adding flag emoji

--- DOM/index.js
+++ DOM/index.js
@@ -8,14 +8,14 @@ document.querySelectorAll("img")[0].setAttribute("src",randomDice1);
 document.querySelectorAll("img")[1].setAttribute("src",randomDice2);
 
 if (randomNum1<randomNum2) {
-	document.querySelector("h1").innerHTML = "Player 2 Wins";
+	document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
 } 
 else {
 	if (randomNum1>randomNum2) {
-		document.querySelector("h1").innerHTML = "Player 1 Wins";
+		document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
 	}
 	else{
-		document.querySelector("h1").innerHTML = "Draw";
+		document.querySelector("h1").innerHTML = "Draw!";
 	}
 }
 
